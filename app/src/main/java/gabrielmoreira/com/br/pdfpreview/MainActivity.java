b564f8b28c80d3562401d036490a5f5e44532826
HalineGabriel <gabriel.moreeira@gmail.com>
2019-01-11 21:44:31 -0200

    Desenvolvido botão para visualizar o arquivo PDF, que foi adicionado na pasta ASSETS.

--- app/src/main/java/gabrielmoreira/com/br/pdfpreview/MainActivity.java
+++ app/src/main/java/gabrielmoreira/com/br/pdfpreview/MainActivity.java
@@ -1,13 +1,33 @@
 package gabrielmoreira.com.br.pdfpreview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_open_assets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_open_assets = findViewById(R.id.btn_open_assets);
+
+        btn_open_assets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ViewActivity.class);
+
+                intent.putExtra("ViewType","assets");
+
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/gabrielmoreira/com/br/pdfpreview/ViewActivity.java
@@ -0,0 +1,93 @@
+package gabrielmoreira.com.br.pdfpreview;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnDrawListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnRenderListener;
+import com.github.barteksc.pdfviewer.listener.OnTapListener;
+
+public class ViewActivity extends AppCompatActivity {
+
+    PDFView pdfView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+
+        pdfView = findViewById(R.id.pdf_viewer);
+
+        if (getIntent() != null)
+        {
+
+            String viewType = getIntent().getStringExtra("ViewType");
+
+            if (viewType != null || !TextUtils.isEmpty(viewType))
+            {
+                if (viewType.equals("assets"))
+                {
+                    pdfView.fromAsset("Desigualdade.pdf")
+                            .password(null)
+                    .defaultPage(0)
+                            .enableSwipe(true)
+                            .swipeHorizontal(false)
+                            .enableDoubletap(true)
+                    .onDraw(new OnDrawListener() {
+                        @Override
+                        public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+
+                        }
+                    })
+
+                            .onDrawAll(new OnDrawListener() {
+                                @Override
+                                public void onLayerDrawn(Canvas canvas, float pageWidth, float pageHeight, int displayedPage) {
+
+                                }
+                            })
+
+                            .onPageError(new OnPageErrorListener() {
+                                @Override
+                                public void onPageError(int page, Throwable t) {
+                                    Toast.makeText(ViewActivity.this,"Error while opne page " + page,Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .onPageChange(new OnPageChangeListener() {
+                                @Override
+                                public void onPageChanged(int page, int pageCount) {
+
+                                }
+                            })
+                            .onTap(new OnTapListener() {
+                                @Override
+                                public boolean onTap(MotionEvent e) {
+                                    return true;
+                                }
+                            })
+                            .onRender(new OnRenderListener() {
+                                @Override
+                                public void onInitiallyRendered(int nbPages, float pageWidth, float pageHeight) {
+                                    pdfView.fitToWidth();
+                                }
+                            })
+
+                            .enableAnnotationRendering(true)
+                            .invalidPageColor(Color.WHITE)
+                            .load();
+                }
+            }
+
+        }
+
+
+    }
+}
